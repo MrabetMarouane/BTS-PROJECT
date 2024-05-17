fe5234d6c27b1a8101de46a9c7d052745b1851dd
Mrabet Marouane <144802049+MrabetMarouane@users.noreply.github.com>
2024-05-17 14:45:51 +0200

    Add files via upload

---
+++ theme/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+    light: {
+        dark: false,
+        colors: {
+            text: 'black',
+            background: 'white',
+        },
+    },
+    dark: {
+        dark: true,
+        colors: {
+            text: 'white',
+            background: 'black',
+        },
+    },
+};
+
+export default theme;
